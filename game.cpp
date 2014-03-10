9008fb47da3f3ca1ccbcf92c30e2a05ee7d6de92
James Chuong <chuong.james@gmail.com>
2014-03-10 16:57:25 -0400

    Added initial game loop for displaying window

--- game.cpp
+++ game.cpp
@@ -1,6 +1,24 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 #include "heli.h"
 
 int main(void)
 {
   // TODO
+  sf::RenderWindow window(sf::VideoMode(700, 500), "Heli-Assault");
+  window.setFramerateLimit(120);
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
+        window.close();
+      }
+    }
+    window.clear();
+    window.display();
+  }
+  return 0;
 }
